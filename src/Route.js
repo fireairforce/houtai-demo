6c6ab3c7507226fabd5ba892c19ca9fd38ebdc70
zoomdong <1344492820@qq.com>
2018-10-19 23:37:55 +0800

    完成了模态框组件的引入

--- src/Route.js
+++ src/Route.js
@@ -4,6 +4,7 @@ import App from './App';
 import Admin from './Admin';
 import Login from './pages/login';
 import Buttons from './pages/ui/buttons';
+import Modals from './pages/ui/modals';
 import NoMatch from './pages/nomatch';
 export default class IRouter extends React.Component{
     render(){
@@ -16,6 +17,7 @@ export default class IRouter extends React.Component{
                          <Admin>
                              <Switch>
                                 <Route path="/admin/ui/buttons" component={Buttons}/>
+                                <Route path="/admin/ui/modals" component={Modals}/>
                                 <Route  component={NoMatch}/>
                              </Switch>   
                          </Admin>    
