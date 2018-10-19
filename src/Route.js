3e595b2af38499dd7741a4d0b3f10f932ca3b8b0
zoomdong <1344492820@qq.com>
2018-10-20 00:24:10 +0800

    学习了加载组件

--- src/Route.js
+++ src/Route.js
@@ -5,6 +5,7 @@ import Admin from './Admin';
 import Login from './pages/login';
 import Buttons from './pages/ui/buttons';
 import Modals from './pages/ui/modals';
+import Loading from './pages/ui/loading';
 import NoMatch from './pages/nomatch';
 export default class IRouter extends React.Component{
     render(){
@@ -18,6 +19,7 @@ export default class IRouter extends React.Component{
                              <Switch>
                                 <Route path="/admin/ui/buttons" component={Buttons}/>
                                 <Route path="/admin/ui/modals" component={Modals}/>
+                                <Route path="/admin/ui/loadings" component={Loading}/>
                                 <Route  component={NoMatch}/>
                              </Switch>   
                          </Admin>    
---
+++ src/pages/ui/loading/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Card,Button ,Spin,Icon,Alert} from 'antd';
+import './../ui.less';
+
+export default class Loading extends React.Component{
+    render(){
+        const icon = <Icon type="plus" style={{fontSize:24}}/>
+        const iconLoading = <Icon type="loading" style={{fontSize:24}}/>
+        return(
+            <div>
+               <Card title="Spin用法" className="card-wrap">
+                   <Spin size="small"/>
+                   <Spin style={{margin:'0 10px'}}/>
+                   <Spin size="large"/>
+                   <Spin indicator={icon} style={{marginLeft:10}} spinning={true}/>
+               </Card>
+               <Card title="内容遮盖" className="card-wrap">
+                    <Alert 
+                       message="ACM俱乐部"
+                       description="欢迎来到NEUQACM俱乐部"
+                       type="info"
+                    />
+                     
+                    <Spin>
+                        <Alert 
+                            message="ACM俱乐部"
+                            description="欢迎来到NEUQACM俱乐部"
+                            type="warning"
+                        />
+                    </Spin>
+
+                     <Spin tip="加载中...">
+                        <Alert 
+                            message="ACM俱乐部"
+                            description="欢迎来到NEUQACM俱乐部"
+                            type="warning"
+                        />
+                    </Spin>
+                    <Spin tip="加载中..." indicator={iconLoading}>
+                        <Alert 
+                            message="ACM俱乐部"
+                            description="欢迎来到NEUQACM俱乐部"
+                            type="warning"
+                        />
+                    </Spin>
+                </Card>   
+            </div>
+        )
+    }
+}
